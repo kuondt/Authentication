346e3d8d8885ed9d2f58b628b662ee7adea7dea5
DaoCuong <cuong.263@gmail.com>
2020-10-23 23:10:08 +0700

    Login & register method

--- Identity/Controllers/HomeController.cs
+++ Identity/Controllers/HomeController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Identity.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Identity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public HomeController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -28,8 +37,14 @@ namespace Identity.Controllers
             return View();
         }
 
-        public IActionResult Login(string username, string password)
+        public async Task<IActionResult> Login(string username, string password)
         {
+            var user = await _userManager.FindByNameAsync(username);
+
+            if(user != null)
+            {
+                //sign in
+            }
             
             return RedirectToAction("Index");
         }
@@ -39,8 +54,22 @@ namespace Identity.Controllers
             return View();
         }
 
-        public IActionResult Register(string username,string password)
+        public async Task<IActionResult> Register(string username,string password)
         {
+            var user = new IdentityUser
+            {
+                UserName = username,
+                Email = "",
+                
+            };
+
+            var result = await _userManager.CreateAsync(user, password);
+
+            if (result.Succeeded)
+            {
+                //sign user
+            }
+
             return RedirectToAction("Index");
         }
     }
